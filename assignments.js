d7bfb925df65bb7ede3b49147e967d889730bb73
Humberto Melean <37026667+HumbertoMelean@users.noreply.github.com>
2023-02-22 17:16:50 -0500

    Complete lecture assignment

--- assignments.js
+++ assignments.js
@@ -1,1 +1,26 @@
 console.log("Assignment File");
+
+// LECTURE: Values and Variables
+let country = "United States";
+let continent = "North American";
+let population = "350,000,000";
+
+// LECTURE: Data Types
+
+// LECTURE: let, const and var
+
+// LECTURE: Basic Operators
+
+// LECTURE: Strings and Template Literals
+
+// LECTURE: Taking Decisions: if / else Statements
+
+// LECTURE: Type Conversion and Coercion
+
+// LECTURE: Equality Operators: == vs. ===
+
+// LECTURE: Logical Operators
+
+// LECTURE: The switch Statement
+
+// LECTURE: The Conditional (Ternary) Operator
