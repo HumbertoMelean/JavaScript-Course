2edb25fc102ab523a5a26817755afb807afba735
Humberto Melean <37026667+HumbertoMelean@users.noreply.github.com>
2023-02-23 14:08:01 -0500

    add complete assignment equality operator

--- assignments.js
+++ assignments.js
@@ -75,9 +75,18 @@ console.log("123" < 57);
 console.log(5 + 6 + "4" + 9 - 4 - 2);
 // 1143
 // LECTURE: Equality Operators: == vs. ===
- */
-
+const numNeighbours = Number(
+  prompt(`How many neighbour countries does your country have?`)
+);
+if (numNeighbours === 0) {
+  console.log(`No borders`);
+} else if (numNeighbours === 1) {
+  console.log(`Only 1 border!`);
+} else {
+  console.log(`More than 1 border`);
+}
 // LECTURE: Logical Operators
+ */
 
 // LECTURE: The switch Statement
 
