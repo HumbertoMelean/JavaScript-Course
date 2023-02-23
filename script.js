7bcfd62a0d820070540c7d2c93d972fd83818392
Humberto Melean <37026667+HumbertoMelean@users.noreply.github.com>
2023-02-22 23:28:57 -0500

    add section 018 notes and code

--- script.js
+++ script.js
@@ -138,6 +138,24 @@ console.log(`String with
 multiple
 lines`);
 
+*/
 // ### 018 Taking Decisions_ if _ else Statements~~~~~~~~~~~~~~
 
-*/
+const age = 15;
+
+if (age >= 18) {
+  console.log(`Sarah can start her driving license exam 🚗`);
+} else {
+  const yearsLeft = 18 - age;
+  console.log(`Sarah is too young. Wait another ${yearsLeft} years.`);
+}
+
+const birthYear = 2012;
+let century;
+if (birthYear <= 2000) {
+  century = 20;
+} else {
+  century = 21;
+}
+
+console.log(century);
