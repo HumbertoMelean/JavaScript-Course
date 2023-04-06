74f15cf7ceb9b1a955034f18ac70a921c65fbc87
Humberto Melean <37026667+HumbertoMelean@users.noreply.github.com>
2023-04-06 19:55:38 -0400

    Completed up to code challenge 2

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -61,9 +61,118 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = '';
+
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? `deposit` : `withdrawal`;
+
+    const html = `
+      <div class="movements__row">
+        <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+        <div class="movements__value">${mov}</div>
+      </div>`;
+
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+displayMovements(account1.movements);
+
+const calcDisplayBalance = function (movements) {
+  const balance = movements.reduce((acc, mov) => (acc += mov), 0);
+  labelBalance.textContent = `${balance} EUR`;
+};
+calcDisplayBalance(account1.movements);
+
+const createUsernames = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+  });
+};
+
+createUsernames(accounts);
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
+
+/////////////////////////////////////////////////
+/*
+// 142 Simple Array Methods~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+let arr = ['a1', 'b2', 'c3', 'd4', 'e5'];
+
+// SLICE
+console.log(arr.slice(2));
+console.log(arr.slice(2, 4));
+console.log(arr.slice(-2));
+console.log(arr.slice(-1));
+console.log(arr.slice(1, -2));
+
+console.log(`~~~~~~~~~~~~~~~~~~~~~~~~`);
+// SPLICE
+// console.log(arr.splice(2));
+arr.splice(-1);
+console.log(arr);
+
+// REVERSE
+arr = ['a1', 'b2', 'c3', 'd4', 'e5'];
+const arr2 = ['5', '4', '3', '2', '1'];
+console.log(arr2.reverse());
+console.log(arr2);
+
+const letters = arr.concat(arr2);
+console.log(letters);
+
+// JOIN
+
+console.log(letters.join(' - '));
+
+
+// 143 The new at Method
+
+const arr = [23, 11, 64];
+console.log(arr[0]);
+console.log(arr.at(0));
+
+// Getting last array element
+console.log(arr.at(-1));
+
+// Works on strings
+console.log('Beto'.at(0));
+console.log('Beto'.at(-1));
+
+// 144 Looping Arrays_ forEach~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// for (const movement of movements) {
+for (const [i, movement] of movements.entries()) {
+  if (movement > 0) {
+    console.log(`Movement ${i + 1}: You deposted ${movement}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
+  }
+}
+
+console.log(`~~~~~~FOR EACH~~~~~`);
+
+movements.forEach(function (mov, i, arr) {
+  if (mov > 0) {
+    console.log(`Movement ${i + 1}: You deposted ${mov}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(mov)}`);
+  }
+});
+
+// 145 forEach With Maps and Sets~~~~~~~~~~~~~~~~~~~~~~
+
+// MAP
 
 const currencies = new Map([
   ['USD', 'United States dollar'],
@@ -71,6 +180,101 @@ const currencies = new Map([
   ['GBP', 'Pound sterling'],
 ]);
 
+currencies.forEach(function (value, key, map) {
+  console.log(`${key}: ${value}`);
+});
+
+const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
+console.log(currenciesUnique);
+currenciesUnique.forEach(function (value, _, map) {
+  console.log(`${value}: ${value}`);
+});
+
+// Coding Challenge 1~~~~~~~~~~~~~~~~~~~~~~~~
+const juliaData1 = [3, 5, 2, 12, 7];
+const juliaData2 = [9, 16, 6, 8, 3];
+
+const kateData1 = [4, 1, 15, 8, 3];
+const kateData2 = [10, 5, 6, 1, 4];
+
+const juliaData1Revised = juliaData1.slice(1, -2);
+console.log(juliaData1Revised);
+
+const combineData1 = juliaData1Revised.concat(kateData1);
+console.log(combineData1);
+
+combineData1.forEach(function (age, i) {
+  if (age < 3) {
+    console.log(`Dog number ${i + 1} is still a puppy`);
+  } else {
+    console.log(`Dog number ${i + 1} is a big boy`);
+  }
+});
+
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-/////////////////////////////////////////////////
+const eurToUsd = 1.1;
+
+// const movementsUSD = movements.map(function (mov) {
+//   return mov * eurToUsd;
+// });
+
+const movementsUSD = movements.map(mov => mov * eurToUsd);
+
+console.log(movements);
+console.log(movementsUSD);
+
+const movementsUSDfor = [];
+for (const mov of movements) movementsUSDfor.push(mov * eurToUsd);
+console.log(movementsUSDfor);
+
+const movemnetsDescription = movements.map(
+  (mov, i) =>
+    `Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrew'} ${Math.abs(
+      mov
+    )}`
+);
+
+console.log(movemnetsDescription);
+
+const deposits = movements.filter(function (mov) {
+  return mov > 0;
+});
+console.log(movements);
+console.log(deposits);
+
+const withdrawals = movements.filter(mov => mov < 0);
+console.log(withdrawals);
+
+console.log(movements);
+
+// Accumulator is like a snowball
+const balance = movements.reduce((acc, cur, i) => (acc += cur), 0);
+
+console.log(balance);
+
+// Maximum Value
+
+const max = movements.reduce((acc, mov) => {
+  if (acc > mov) return acc;
+  else return mov;
+}, movements[0]);
+
+console.log(max);
+
+
+const juliasData = [5, 2, 4, 1, 15, 8, 3];
+const katesData = [16, 6, 10, 5, 6, 1, 4];
+
+const calcAverageHumanAge = function (ages) {
+  const humanAge = ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
+  const adults = humanAge.filter(age => age >= 18);
+  const average = adults.reduce((acc, age) => (acc += age), 0) / adults.length;
+  return average;
+};
+
+const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+
+console.log(avg1, avg2);
+*/
--- operators.md
+++ operators.md
@@ -30,3 +30,5 @@
 - includes()
 
 .textContent
+
+.insertAdjacentHTML
