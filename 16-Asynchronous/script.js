bd53a12e646f287d6cc3554356cd36aa8aa9f35b
Humberto Melean <37026667+HumbertoMelean@users.noreply.github.com>
2023-04-27 15:37:44 -0400

    Start section 17

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -4,3 +4,61 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+
+const renderCountry = function (data, className = '') {
+  const html = ` 
+  <article class="${className}">
+    <img class="country__img" src="${Object.values(data.flags).find(flags =>
+      flags.endsWith('.png')
+    )}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name.common}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(
+        +data.population / 1000000
+      ).toFixed(1)}</p>
+      <p class="country__row"><span>🗣️</span>${
+        Object.values(data.languages)[0]
+      }</p>
+      <p class="country__row"><span>💰</span>${
+        Object.values(data.currencies)[0].name
+      }</p>
+    </div>
+  </article>`;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryAndNeighbor = function (country) {
+  // Ajax call country
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    // Render Country 1
+    renderCountry(data);
+
+    // Get neighbor country
+    const [neighbor] = data.borders;
+
+    if (!neighbor) return;
+
+    // Ajax call country 2
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbor}`);
+    request2.send();
+
+    request2.addEventListener('load', function () {
+      const data2 = JSON.parse(this.responseText);
+      console.log(data2);
+
+      renderCountry(data2);
+    });
+  });
+};
+
+getCountryAndNeighbor('usa');
---
+++ 17-Modern-JS-Modules-Tooling/script.js
@@ -0,0 +1,8 @@
+// Importing Module
+import './shoppingCart.js';
+console.log('Importing Module');
+
+const shippingCost = 10;
+const cart = [];
+
+console.log(shippingCost);
